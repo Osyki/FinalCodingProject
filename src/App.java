60654fa22409976da22b1589bf77b2c4d720a94a
Jonathan Rubio <jon.rubio.92@gmail.com>
2022-04-01 18:38:59 -0400

    Changed print function to show each round

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        Player player1 = new Player("Player1");
-        Player player2 = new Player("Player2");
+        Player player1 = new Player("John");
+        Player player2 = new Player("Jane");
         Deck deck1 = new Deck();
         deck1.shuffle();
 
@@ -14,23 +14,33 @@ public class App {
             }
         }
 
-        player1.describe();
-        player2.describe();
+//        player1.describe();
+//        player2.describe();
 
         //run simulation
         for (int i = 0; i < 26; i++) {
             Card p1DrawnCard = new Card(player1.flip());
             Card p2DrawnCard = new Card(player2.flip());
 
+            p1DrawnCard.describe();
+            System.out.print(" vs ");
+            p2DrawnCard.describe();
+
             if (p1DrawnCard.getValue() > p2DrawnCard.getValue()) {
+                System.out.println("\r\n" + player1.getName() + "'s score +1");
                 player1.incrementScore();
             } else if (p2DrawnCard.getValue() > p1DrawnCard.getValue()) {
+                System.out.println("\r\n" + player2.getName() + "'s score +1");
                 player2.incrementScore();
+            } else {
+                System.out.println("Draw");
             }
+
+            System.out.print("\r\n");
         }
 
         //print final scores
-        String finalScoreText = "\r\n****FINAL SCORE:****\r\n" + "    " + player1.getName() + ": " + player1.getScore() + "\r\n" + "    " + player2.getName() + ": " + player2.getScore() + "\r\n" + "WINNER: ";
+        String finalScoreText = "****FINAL SCORE:****\r\n" + "    " + player1.getName() + ": " + player1.getScore() + "\r\n" + "    " + player2.getName() + ": " + player2.getScore() + "\r\n" + "WINNER: ";
         if (player1.getScore() > player2.getScore()) {
             finalScoreText += player1.getName();
         } else if (player2.getScore() > player1.getScore()) {
